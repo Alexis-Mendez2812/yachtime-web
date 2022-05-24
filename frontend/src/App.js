39db0af1c194ddf3146ddbe05b4884a5dd456d50
Alexis-Mendez2812 <281212.namaste@gmail.com>
2022-05-23 21:47:39 -0300

    Profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,18 +6,21 @@ import Navbar from "./Components/Navbar/Navbar"
 import { LoginForm } from "./Components/Login/LoginForm.jsx";
 import { RegisterForm } from "./Components/Login/RegisterForm.jsx";
 import  Loading  from "./Components/Loading/Loading.jsx";
+import { Profile } from "./Components/Login/Profile.jsx";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path='/profile' element={<Profile />} />
           <Route path='/loading' element={<Loading />} />
           <Route path='/login' element={<Login />} />
           <Route path='/loginForm' element={<LoginForm />} />
           <Route path='/registerForm' element={<RegisterForm />} />
           <Route exact path='/' element={<Home />} />
           <Route path="/" element={<Navbar />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/Components/Login/Profile.jsx
+++ frontend/src/Components/Login/Profile.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-import { useAuth0 } from "@auth0/auth0-react";
+import { useSelector } from "react-redux";
+import Loading from "../Loading/Loading";
 // import { useDispatch } from "react-redux";
 
 export const Profile = () => {
 
   // const dispatch = useDispatch();
-  const {user, isLoading, isAuthenticated} = useAuth0();
 
-  //  const favoritesCharacters = useSelector((state) => state.CharactersReducer.favoritesCharacters)
+   const userSession = useSelector((state) => state.userSession)
   
  
     // useEffect(() => {
@@ -19,21 +19,19 @@ export const Profile = () => {
 
 
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-  console.log(user)
   return (
-    isAuthenticated && (
+    userSession.email?(
       <div className="Profile-Card">
-        <img src={user.picture} alt={user.name} />
-        <h2>User Name:</h2>
-        <h2>{user.nickname}</h2>
+        <img src={userSession.picture} alt={userSession.name} />
+        <h2> Name:</h2>
+        <h2>{userSession.nickname}</h2>
         <h4>Name:</h4>
-        <h4>{user.name}</h4>
-        <p>Email: {user.email}</p>
+        <h4>{userSession.name}</h4>
+        <p>Email: {userSession.email}</p>
 
       </div>
+    ):(
+      <Loading/>
     )
   );
 };
