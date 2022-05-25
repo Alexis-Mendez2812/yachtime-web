e45e42a6c4c4c3221455236ed43468176005185e
Alexis-Mendez2812 <281212.namaste@gmail.com>
2022-05-25 14:28:05 -0300

    version estable

--- frontend/src/Components/Login/LoginButton.jsx
+++ frontend/src/Components/Login/LoginButton.jsx
@@ -1,25 +1,15 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useDispatch } from "react-redux";
-import {postUserGoogle} from "../../Redux/Actions/actions.js"
 
 export const LoginButton = () => {
-  const dispatch = useDispatch();
-  const { loginWithRedirect ,user } = useAuth0();
 
-  const loginWithGoogle = async function(){ 
-    await loginWithRedirect();
-    
-    }
-    useEffect(() => {
-   
-      if(user?.email){ return dispatch(postUserGoogle(user))
-      }
-    }, [dispatch,user]);
+  const { loginWithRedirect  } = useAuth0();
+
+
 
   return(<>
   <div className="" >
-  <button onClick={loginWithGoogle}>
+  <button onClick={loginWithRedirect}>
     <img src="https://www.yachtlife.com/static/media/google_icon.795cf39d.svg" alt="google" ></img>  
       CONTINUE WITH GOOGLE</button>
   </div>
