ffbce8889e81ab5c84f2a8d7aa04f85a24ef6745
M0n00s <programacion.buffalo@gmail.com>
2022-05-31 17:01:51 -0400

    1

--- frontend/src/Components/Uploading/Uploading.jsx
+++ frontend/src/Components/Uploading/Uploading.jsx
@@ -1,64 +1,66 @@
-import React,{ useState } from 'react'
-import style from "./Uploading.module.css"
-import axios from "axios"
-import { CloudinaryContext, Image } from 'cloudinary-react'
-
-
+import React, { useState } from "react";
+import style from "./Uploading.module.css";
+import axios from "axios";
+import { CloudinaryContext, Image } from "cloudinary-react";
 
 function Uploading() {
+	const [previewSource, setPreviewSource] = useState("");
+	const [fileInputState, setFileInputState] = useState("");
+	const [selectedFile, setSelectedFile] = useState("");
 
+	const handleFileInputChange = (e) => {
+		const file = e.target.files[0];
+		previewFile(file);
+	};
 
-const [previewSource,setPreviewSource]= useState("")
-const [fileInputState,setFileInputState]= useState("")
-const [selectedFile,setSelectedFile]= useState("")
+	const previewFile = (file) => {
+		const reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onloadend = () => {
+			setPreviewSource(reader.result);
+		};
+	};
 
+	const handleSubmitFile = (e) => {
+		e.preventDefault();
+		if (!previewSource) return;
+		uploadImage(previewSource);
+	};
 
-
-const handleFileInputChange =(e)=> {
-        const file =e.target.files[0];
-        previewFile(file);
+	const uploadImage = async (base64EncodeImage) => {
+		console.log(base64EncodeImage);
+		try {
+			await axios.post("/api/upload", {
+				data: JSON.stringify({ data: base64EncodeImage }),
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	return (
+		<div className={style.uploadingContainer}>
+			<h1>Upload</h1>
+			<form onSubmit={handleSubmitFile}>
+				<input
+					type="file"
+					name="image"
+					onChange={handleFileInputChange}
+					value={fileInputState}
+					className="uploagind-input"
+				/>
+				<button className="btn" type="submit"></button>
+			</form>
+			<div>
+				{previewSource && (
+					<img
+						src={previewSource}
+						alt="chosen"
+						style={{ height: "20%", width: "auto" }}
+					/>
+				)}
+			</div>
+		</div>
+	);
 }
 
-const previewFile = (file)=>{
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onloadend=()=>{
-        setPreviewSource(reader.result);
-}
-}
-
-const handleSubmitFile=(e)=>{
-    e.preventDefault();
-    if(!previewSource)return;
-uploadImage(previewSource);
-}
-
-const uploadImage= async (base64EncodeImage)=> {
-console.log(base64EncodeImage);
-try {
-    await axios.post("/api/upload",{
-      data: JSON.stringify({data:base64EncodeImage})
-    })
-} catch (error) {
-    console.log(error)
-}
-}
-  return (
-    <div className={style.uploadingContainer}>
-        <h1>Upload</h1>
-        <form onSubmit={handleSubmitFile} >
-            <input type="file" name="image" onChange={handleFileInputChange} value={fileInputState} className="uploagind-input"  />
-            <button  className="btn" type="submit" ></button>
-        </form>
-        <div>
-        {previewSource && (
-          <img src={previewSource} alt="chosen"  style={{height:"20%",width:"auto"}} />
-          )}
-          </div>
-        
-        
-    </div>
-  )
-}
-
-export default Uploading
+export default Uploading;
