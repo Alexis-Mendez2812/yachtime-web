0703cd8955421d4c9136906ae6aa6c3f68f0c3be
M0n00s <programacion.buffalo@gmail.com>
2022-06-01 20:00:16 -0400

    BannerStyles

--- frontend/src/Components/BannerHome/BannerHome.jsx
+++ frontend/src/Components/BannerHome/BannerHome.jsx
@@ -1,40 +1,57 @@
 import React, { useState } from "react";
 import style from "./BannerHome.module.css";
-import { MdOutlineSearch } from "react-icons/md"
+import { MdOutlineSearch } from "react-icons/md";
 
 function BannerHome() {
-  const [state , setState] = useState("Maimi(FL)")
-  const handleChange = (event) => {
-    setState(event.target.value)
-  }
-  return (
-    <div className={style.bannerHome}>
-      <div className={style.banner}>
-        <span className={style.city}>{state}</span>
-        <div className={style.select}>
-          <div className={style.size}>
-            <h3>Size:</h3>
-            <select onChange={(event) => handleChange(event)}>
-              <option value="Miami(FL)">Miami</option>
-              <option value="Chicago(IL)">Chicago</option>
-              <option value="Las Vegas(NV )">Las Vegas</option>
-            </select>
-          </div>
-          <div className={style.duration}>
-            <h3>Duration:</h3>
-            <select>
-              <option>Miami</option>
-              <option>Chicago</option>
-              <option>Las Vegas</option>
-            </select>
-          </div>
-          <div className={style.iconSearch}>
-            <MdOutlineSearch/>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const [state, setState] = useState("Maimi(FL)");
+	const handleChange = (event) => {
+		setState(event.target.value);
+	};
+	return (
+		<div className={style.bannerHome}>
+			<div className={style.banner}>
+				<div className={style.bannerText}>
+					<h3 className={style.charter}>Charter A Yacht</h3>
+					<p>
+						<span className={style.city}>
+							<br />
+							{state}
+						</span>
+					</p>
+				</div>
+				<div className={style.select}>
+					<div className={style.size}>
+						<h3>Location:</h3>
+						<select onChange={(event) => handleChange(event)}>
+							<option value="select">Select</option>
+							<option value="Miami(FL)">Miami</option>
+							<option value="Chicago(IL)">Chicago</option>
+							<option value="Las Vegas(NV)">Las Vegas</option>
+						</select>
+					</div>
+					<div className={style.duration}>
+						<h3>Duration:</h3>
+						<select>
+							<option value="select">Select</option>
+							<option value="Half Day (4hrs)">
+								Half Day (4hrs)
+							</option>
+							<option value="Full Day (8hrs)">
+								Full Day (8hrs)
+							</option>
+							<option value="2-6 Days">2-6 Days</option>
+							<option value="7 Days or More">
+								7 Days or More
+							</option>
+						</select>
+					</div>
+					<div className={style.iconSearch}>
+						<MdOutlineSearch />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default BannerHome;
--- frontend/src/Components/Membership/Membership.jsx
+++ frontend/src/Components/Membership/Membership.jsx
@@ -1,83 +1,134 @@
 import React from "react";
 import { SiInstagram } from "react-icons/si";
-import Navbar from '../Navbar/Navbar';
-import tarjet from '../Membership/img/business_card.png';
-import './Membership.css';
-import Footer from '../Footer/Footer'
+import Navbar from "../Navbar/Navbar";
+import tarjet from "../Membership/img/business_card.png";
+import background from "./img/banner_menber_01.jpg";
+import "./Membership.css";
+import Footer from "../Footer/Footer";
 function Membership() {
-    return (
-      <>
-      <Navbar />
+	return (
+		<>
+			<Navbar />
 
-      <div class="about">
-        <div class="inner-section">
-           <img className="img" src={tarjet} alt='img' width= '450px' height= '450px'></img>
-        
-            <h1 className="how">How To Join</h1>
-            <p class="text">
-            Yachtimeapp welcomes all people who love being on the water, no matter where they are in the world.
-            </p>
-            <p class="text">Each prospective member is asked to submit a simple application and will have a phone call with our membership specialist to review their yacht charter patterns and upcoming needs to ensure a mutual fit.</p>
+			<div class="about">
+				<div>
+					<img
+						src={background}
+						alt="img"
+						width="450px"
+						height="450px"
+					></img>
+				</div>
+				<div class="inner-section">
+					<img
+						className="img"
+						src={tarjet}
+						alt="img"
+						width="450px"
+						height="250px"
+					></img>
 
-            <div class="skills">
-                <button>GET IR NOW</button>
-            </div>
-        </div>
-    </div>
+					<h1 className="how">How To Join</h1>
+					<p class="text">
+						Yachtimeapp welcomes all people who love being on the
+						water, no matter where they are in the world.
+					</p>
+					<p class="text">
+						Each prospective member is asked to submit a simple
+						application and will have a phone call with our
+						membership specialist to review their yacht charter
+						patterns and upcoming needs to ensure a mutual fit.
+					</p>
 
-    <div>
-    <div class="title-cards">
-        
-    <h1 className="titulo">Our Memberships</h1>
-		
-    <p class="text">
-    Yachttimeapp offers three membership tiers. Members pay a one-time initiation fee and an annual fee to access a suite of benefits including:
-            </p>
-	</div>
-<div class="container-card">
-	
-<div class="card">
-	<div class="contenido-card">
-		<h2 className="listah2">ANNUAL</h2>
-		<p>Individuals that typically entertain family and friends or clients on single day charters throughout the year.</p>
-		<div>
-        <ul className="listaul">
-            <li><p>Offer your yacht with us.</p></li>
-            <li><p>You will not pay commissions for each charter.</p></li>
-            <li><p>The best yacht's selection of all Miami.</p></li>
-            <li><p>Take advantage of our promos.</p></li>
-        </ul>
-  </div>   
-        <a href="/paypal/pay/300">300$ / Annual</a>
-	</div>
-</div>
-<div class="card">
-	<div class="contenido-card">
-		<h2 className="listah2">MONTHLY</h2>
-		<p>Individuals that typically entertain family and friends or clients on single day charters throughout the year.</p>
-     <div>
-        <ul className="listaul">
-            <li><p>Offer your yacht with us.</p></li>
-            <li><p>You will not pay commissions for each charter.</p></li>
-            <li><p>The best yacht's selection of all Miami.</p></li>
-            <li><p>Take advantage of our promos.</p></li>
-        </ul>
-  </div>   
-		<a href="/paypal/pay/75">75$ / Month</a>       
-	</div>
-</div>
+					<div class="skills">
+						<button>GET IT NOW</button>
+					</div>
+				</div>
+			</div>
 
-</div> 
-    </div>
+			<div>
+				<div class="title-cards">
+					<h1 className="titulo">Our Memberships</h1>
 
-   
+					<p class="text">
+						Yachttimeapp offers three membership tiers. Members pay
+						a one-time initiation fee and an annual fee to access a
+						suite of benefits including:
+					</p>
+				</div>
+				<div class="container-card">
+					<div class="card">
+						<div class="contenido-card">
+							<h2 className="listah2">ANNUAL</h2>
+							<p>
+								Individuals that typically entertain family and
+								friends or clients on single day charters
+								throughout the year.
+							</p>
+							<div>
+								<ul className="listaul">
+									<li>
+										<p>Offer your yacht with us.</p>
+									</li>
+									<li>
+										<p>
+											You will not pay commissions for
+											each charter.
+										</p>
+									</li>
+									<li>
+										<p>
+											The best yacht's selection of all
+											Miami.
+										</p>
+									</li>
+									<li>
+										<p>Take advantage of our promos.</p>
+									</li>
+								</ul>
+							</div>
+							<a href="/paypal/pay/300">300$ / Annual</a>
+						</div>
+					</div>
+					<div class="card">
+						<div class="contenido-card">
+							<h2 className="listah2">MONTHLY</h2>
+							<p>
+								Individuals that typically entertain family and
+								friends or clients on single day charters
+								throughout the year.
+							</p>
+							<div>
+								<ul className="listaul">
+									<li>
+										<p>Offer your yacht with us.</p>
+									</li>
+									<li>
+										<p>
+											You will not pay commissions for
+											each charter.
+										</p>
+									</li>
+									<li>
+										<p>
+											The best yacht's selection of all
+											Miami.
+										</p>
+									</li>
+									<li>
+										<p>Take advantage of our promos.</p>
+									</li>
+								</ul>
+							</div>
+							<a href="/paypal/pay/75">75$ / Month</a>
+						</div>
+					</div>
+				</div>
+			</div>
 
-    <Footer/>
+			<Footer />
+		</>
+	);
+}
 
-	
-      </>
-    );
-  }
-  
-  export default Membership;
-  
+export default Membership;
