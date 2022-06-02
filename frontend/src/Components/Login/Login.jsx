f126641d38426819019e94b6de03479e8152b1ac
M0n00s <programacion.buffalo@gmail.com>
2022-06-02 11:13:20 -0400

    logComponentStart

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,38 +1,40 @@
-import React from 'react'
+import React from "react";
 
 import { LoginButton } from "./LoginButton.jsx";
 
-import "./Profile.css"
-import style from "./Login.module.css"
-import { Link } from 'react-router-dom';
+import "./Profile.css";
+import style from "./Login.module.css";
+import { Link } from "react-router-dom";
 
 function Login() {
-
-
-
-  return (
-    <>
-        <div className={style.container}></div>
-      <div className={style.header}>
-
-        {(
-          <div>
-            <div>
-            <div>
-    <h1>Welcome to Yach Time App</h1>
-    <h3>Choose an option to continue</h3>
-  </div>
-            </div>
-          <LoginButton />
-            <Link to={"/LoginForm"} ><button>CONTINUE WITH YACH COUNT</button></Link>
-            <Link to={"/RegisterForm"} ><button> REGISTER WITH YACH COUNT</button></Link>
-            <Link to={"/"} ><button>BACK TO HOME</button></Link>
-          </div>
-        )}
-      </div>
-      </>
-
-  );
+	return (
+		<>
+			<div className={style.container}>
+				<div className={style.header}>
+					{
+						<div>
+							<div>
+								<div>
+									<h1>Welcome to Yach Time App</h1>
+									<h3>Choose an option to continue</h3>
+								</div>
+							</div>
+							<LoginButton />
+							<Link to={"/LoginForm"}>
+								<button>CONTINUE WITH YACH COUNT</button>
+							</Link>
+							<Link to={"/RegisterForm"}>
+								<button> REGISTER WITH YACH COUNT</button>
+							</Link>
+							<Link to={"/"}>
+								<button>BACK TO HOME</button>
+							</Link>
+						</div>
+					}
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Login;
--- frontend/src/Components/Login/LoginButton.jsx
+++ frontend/src/Components/Login/LoginButton.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export const LoginButton = () => {
+	const { loginWithRedirect } = useAuth0();
 
-  const { loginWithRedirect  } = useAuth0();
-
-
-
-  return(<>
-  <div className="" >
-  <button onClick={loginWithRedirect}>
-    <img src="https://www.yachtlife.com/static/media/google_icon.795cf39d.svg" alt="google" ></img>  
-      CONTINUE WITH GOOGLE</button>
-  </div>
-  </> 
-  );
+	return (
+		<>
+			<div className="">
+				<button onClick={loginWithRedirect}>
+					<img
+						src="https://www.yachtlife.com/static/media/google_icon.795cf39d.svg"
+						alt="google"
+					></img>
+					CONTINUE WITH GOOGLE
+				</button>
+			</div>
+		</>
+	);
 };
