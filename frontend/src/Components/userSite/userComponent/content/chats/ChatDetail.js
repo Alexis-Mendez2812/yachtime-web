d022a4dac168bbfe80cc99d11c73bf0cb2e5259b
AlejoBengo <henryaskhenry@gmail.com>
2022-06-06 00:47:15 -0300

    vd

--- frontend/src/Components/userSite/userComponent/content/chats/ChatDetail.js
+++ frontend/src/Components/userSite/userComponent/content/chats/ChatDetail.js
@@ -1,68 +1,123 @@
-import React, { useState } from 'react';
-import './chatDetail.css';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { bringAllChats } from '../../../../../Redux/Actions/ChatActions/chatActions';
+import { getAllUsers } from '../../../../../Redux/Actions/actions';
+import MessageIcon from '@mui/icons-material/Message';
+import Loading from '../../../../Loading/Loading';
 import { Box } from '@mui/material';
+import {
+   Container,
+   HistoryBox,
+   Input,
+   Title,
+   UserNameGraphy,
+   MessageBox,
+   LastMessGraphy,
+   EachChatBox,
+   SendButton,
+   ChatContainer,
+   ChatBox,
+} from './styledComponents';
 
 export const ChatDetail = () => {
-   const [chats, setChats] = useState(false);
+   const dispatch = useDispatch();
+   let { userSession, users } = useSelector((state) => {
+      return state;
+   });
+   const [adminId, setAdminId] = useState('');
+   const [allMyChats, setAllMyChats] = useState([]);
+   const [charging, setCharging] = useState(true);
+
+   useEffect(() => {
+      dispatch(getAllUsers());
+   }, []);
+
+   useEffect(() => {
+      setAdminId(userSession.id);
+   }, [userSession.id]);
+
+   useEffect(() => {
+      if (adminId && users.length) {
+         let prueba = {};
+         let eachChat = [];
+         bringAllChats(adminId)
+            .then((ans) => {
+               for (let i = 0; i < ans.length; i++) {
+                  if (!Object.keys(prueba).includes(ans[i].UserId)) {
+                     prueba[ans[i].UserId] = [ans[i].message];
+                  } else {
+                     prueba[ans[i].UserId] = [
+                        ...prueba[ans[i].UserId],
+                        ans[i].message,
+                     ];
+                  }
+               }
+               for (let prop in prueba) {
+                  const actualUser = users.find((u) => {
+                     return u.id === prop;
+                  });
+                  eachChat.push({
+                     name: actualUser.userName,
+                     mess: prueba[prop],
+                  });
+               }
+               console.log('ACA: ', eachChat);
+               setAllMyChats(eachChat);
+            })
+            .catch((err) => console.log(err));
+      }
+   }, [adminId, users]);
+
+   const handleOpenChat = (e) => {
+      setCharging(!charging);
+   };
 
    return (
-      <Box
-         style={{
-            backgroundColor: 'red',
-            height: '100vh',
-            width: '81.7vw',
-            display: 'flex',
-         }}
-      >
-         <Box
-            style={{ backgroundColor: 'green', height: '100vh', width: '30vw' }}
-         ></Box>{' '}
-         <Box
-            style={{
-               backgroundColor: 'purple',
-               height: '100vh',
-               width: '51.7vw',
-            }}
-         >
-            <Box
-               style={{
-                  backgroundColor: 'brown',
-                  height: '93vh',
-                  width: '51.7vw',
-               }}
-            ></Box>
-            <Box
-               style={{
-                  backgroundColor: 'blue',
-                  height: '7vh',
-                  width: '51.7vw',
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'space-around',
-               }}
-            >
-               <input
+      <Container>
+         <HistoryBox>
+            <Title>_____ Your Chats _____</Title>
+            {allMyChats.length > 0 &&
+               allMyChats.map((m) => {
+                  return (
+                     <EachChatBox key={m.name} onClick={handleOpenChat}>
+                        <MessageIcon color='primary' fontSize='large' />
+                        <Box
+                           style={{
+                              marginLeft: '0.4rem',
+                              width: '90%',
+                              height: '100%',
+                              overflow: 'hidden',
+                           }}
+                        >
+                           <UserNameGraphy>{m.name}</UserNameGraphy>
+                           <LastMessGraphy>{m.mess[0]}</LastMessGraphy>
+                        </Box>
+                     </EachChatBox>
+                  );
+               })}
+         </HistoryBox>
+         <ChatContainer>
+            {charging ? (
+               <Box
                   style={{
-                     backgroundColor: 'white',
-                     width: '85%',
-                     height: '80%',
-                     border: '0vh',
-                     outline: 'none',
-                  }}
-               ></input>
-               <button
-                  style={{
-                     backgroundColor: 'yellow',
-                     color: 'black',
-                     width: '11%',
-                     height: '50%',
-                     border: '0vh',
+                     backgroundColor: 'black',
+                     display: 'flex',
+                     justifyContent: 'center',
+                     alignItems: 'center',
                   }}
                >
-                  Send
-               </button>
-            </Box>
-         </Box>
-      </Box>
+                  <Loading />
+               </Box>
+            ) : (
+               <Box>
+                  <ChatBox></ChatBox>
+                  <MessageBox>
+                     <Input></Input>
+                     <SendButton>Send</SendButton>
+                  </MessageBox>
+               </Box>
+            )}
+         </ChatContainer>
+      </Container>
    );
 };
--- frontend/src/Redux/Actions/ChatActions/chatActions.js
+++ frontend/src/Redux/Actions/ChatActions/chatActions.js
@@ -27,3 +27,17 @@ export const bringAllMessages = async ({ owner, receptor }) => {
       return 'No';
    }
 };
+
+export const bringAllChats = async (to) => {
+   try {
+      const res = await axios(`/chat/all?to=${to}`);
+      if (res) {
+         return res.data;
+      } else {
+         return 'No';
+      }
+   } catch (err) {
+      console.log(err);
+      return 'No';
+   }
+};
