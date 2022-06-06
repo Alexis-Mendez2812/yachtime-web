c0e130d21063fe7022e411a0292977010e3498e7
Alejo Bengo <93496125+AlejoBengo@users.noreply.github.com>
2022-06-06 15:55:49 -0300

    Update CardDetail.jsx

--- frontend/src/Components/CardDetail/CardDetail.jsx
+++ frontend/src/Components/CardDetail/CardDetail.jsx
@@ -34,7 +34,7 @@ import {
 import { Box, TextField } from '@mui/material';
 import { getIdYate, vaciar } from '../../Redux/Actions/actions';
 import io from 'socket.io-client';
-const socket = io.connect('https://yachtimeapp.herokuapp.com');
+const socket = io.connect();
 
 export default function GameDetail() {
    const { id } = useParams();
