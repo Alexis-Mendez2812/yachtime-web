75bd8a5f1796a2002a9a196bc675700a86bac834
Alejo Bengo <93496125+AlejoBengo@users.noreply.github.com>
2022-06-06 15:18:43 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
-const server = require('./src/app.js');
+const {io, } = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 conn.sync({ force: false }).then(() => {
    server.listen(process.env.PORT, () => {
       console.log(`%s listening at ${process.env.PORT}`);
+   });
+});
+
+
+io.on('connection', (socket) => {
+   socket.on('join_chat', (room) => {
+      socket.join(room);
+   });
+
+   socket.on('send_message', (data) => {
+      socket.to(data.room).emit('receive_message', data.actualMessage);
    });
 });
