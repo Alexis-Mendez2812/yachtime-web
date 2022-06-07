9c2588481e5c8cce794fcfc75142c1ef32494793
M0n00s <programacion.buffalo@gmail.com>
2022-06-07 10:33:59 -0400

    delete rute add

--- frontend/src/Redux/Actions/ProductActions/getAllProducts.js
+++ frontend/src/Redux/Actions/ProductActions/getAllProducts.js
@@ -1,21 +1,30 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const getAllProducts = () => {
-   return (dispatch) => {
-      axios('/product/all')
-         .then((ans) => {
-            return dispatch({ type: 'ALL_YATES', payload: ans.data });
-         })
-         .catch((err) => console.log(err));
-   };
+	return (dispatch) => {
+		axios("/product/all")
+			.then((ans) => {
+				return dispatch({ type: "ALL_YATES", payload: ans.data });
+			})
+			.catch((err) => console.log(err));
+	};
 };
 
 export const getProductById = (id) => {
-   return (dispatch) => {
-      axios(`/products/detail/${id}`)
-         .then((ans) => {
-            return dispatch({ type: 'GET_YATE_DETAIL', payload: ans.data });
-         })
-         .catch((err) => console.log(err));
-   };
+	return (dispatch) => {
+		axios(`/products/detail/${id}`)
+			.then((ans) => {
+				return dispatch({ type: "GET_YATE_DETAIL", payload: ans.data });
+			})
+			.catch((err) => console.log(err));
+	};
+};
+
+export const deleteYacht = (yateID) => {
+	return (dispatch) => {
+		axios
+			.delete(`http://localhost:3000/products`, yateID)
+			.then((data) => data.data)
+			.catch((err) => console.log(err));
+	};
 };
