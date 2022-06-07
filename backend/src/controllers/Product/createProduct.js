30b52de456c6492f881ccf0c6530b8dec7343fb9
AlejoBengo <henryaskhenry@gmail.com>
2022-06-07 00:19:38 -0300

    [FIX] Post Form Route

--- backend/src/controllers/Product/createProduct.js
+++ backend/src/controllers/Product/createProduct.js
@@ -6,87 +6,59 @@ cloudinary.config({
    api_key: process.env.CLOUDINARY_API_KEY,
    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-const { botes } = require('../../yates.json');
 
 const uploadImage = async (req, res) => {
    try {
-      const { owner } = req.body;
-      botes.forEach(async (e) => {
-         const [save] = await Products.findOrCreate({
-            where: {
-               make: e.make,
-               model: e.model,
-               year: e.year,
-               cabins: e.cabins,
-               bathrooms: e.bathrooms,
-               guests: e.guests,
-               length: e.length,
-               beam: e.beam,
-               draft: e.draft,
-               fuelCapacity: e.fuelCapacity,
-               waterCapacity: e.waterCapacity,
-               cruiseVel: e.cruiseVel,
-               fuelType: e.fuelType ? e.fuelType : 'gasoline',
-               description: e.description,
-               pictures: e.pictures,
-            },
+      const {
+         make,
+         model,
+         year,
+         cabins,
+         bathrooms,
+         guest,
+         length,
+         beam,
+         draft,
+         fuelCapacity,
+         waterCapacity,
+         cruiseVel,
+         location,
+         fuelType,
+         description,
+         pictures,
+         owner,
+      } = req.body;
+
+      var URLs = [];
+      for (let i = 0; i < pictures.length; i++) {
+         const urlimg = await cloudinary.uploader.upload(pictures[i], {
+            upload_preset: 'api-img',
          });
-
-         const user = await Users.findOne({ where: { id: owner } });
-         await user.addProducts(save);
+         URLs.push(urlimg.secure_url);
+      }
+      const [newProduct, bool] = await Products.findOrCreate({
+         where: {
+            make,
+            model,
+            year,
+            cabins,
+            bathrooms,
+            guests: guest,
+            length,
+            beam,
+            draft,
+            fuelCapacity,
+            waterCapacity,
+            cruiseVel,
+            location,
+            fuelType,
+            description,
+            pictures: URLs,
+         },
       });
-      //    const {
-      //       make,
-      //       model,
-      //       year,
-      //       cabins,
-      //       bathrooms,
-      //       guest,
-      //       length,
-      //       beam,
-      //       draft,
-      //       fuelCapacity,
-      //       waterCapacity,
-      //       cruiseVel,
-      //       location,
-      //       fuelType,
-      //       description,
-      //       pictures,
-      //       owner,
-      //    } = req.body;
 
-      //    var URLs = [];
-
-      //    for (let i = 0; i < pictures.length; i++) {
-      //       const urlimg = await cloudinary.uploader.upload(pictures[i], {
-      //          upload_preset: 'api-img',
-      //       });
-      //       URLs.push(urlimg.secure_url);
-      //       if (i === pictures.length - 1) {
-      //          const [newProduct, bool] = await Products.findOrCreate({
-      //             where: {
-      //                make,
-      //                model,
-      //                year,
-      //                cabins,
-      //                bathrooms,
-      //                guests: guest,
-      //                length,
-      //                beam,
-      //                draft,
-      //                fuelCapacity,
-      //                waterCapacity,
-      //                cruiseVel,
-      //                location,
-      //                fuelType,
-      //                description,
-      //                pictures: URLs,
-      //             },
-      // //          });
-      // const user = await Users.findOne({ where: { id: owner } });
-      // await user.addProducts(newProduct);
-      //    }
-      // }
+      const user = await Users.findOne({ where: { id: owner } });
+      await user.addProducts(newProduct.id);
 
       res.json({ status: 'Product saved successfully.' });
    } catch (err) {
