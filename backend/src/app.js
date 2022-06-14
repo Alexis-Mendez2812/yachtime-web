c0ea73f5bc64e7c39996ea3178bc0dc434ee0ef3
Alejo Bengo <93496125+AlejoBengo@users.noreply.github.com>
2022-06-14 00:30:23 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -44,7 +44,7 @@ app.use((err, req, res, next) => {
 const newServer = http.createServer(app);
 const io = new Server(newServer, {
    cors: {
-      origin: 'https://yachtimeapp.com'
+      origin: 'http://yachtimeapp.com'
    },
 });
 
