a38b0dfa3b8d68d7bab5c0cc5ee6ccc60f270341
Alexis-Mendez2812 <281212.namaste@gmail.com>
2022-06-16 00:17:34 -0300

    filtros

--- frontend/src/Components/Filtros/Filtros.jsx
+++ frontend/src/Components/Filtros/Filtros.jsx
@@ -25,38 +25,25 @@ const Filtros = ({ setCurrentPage, setCharging }) => {
 
    const [sizeSelect, setSizeSelect] = useState('All');
    const [modelSelect, setModelSelect] = useState('All');
-   const [yearSelect, setYearSelect] = useState('All');
    const [guestsSelect, setGuestsSelect] = useState('All');
-   const [lengthSelect, setLengthSelect] = useState('All');
 
    let sizeArr = [];
    let modelArr = [];
-   let yearArr = [];
    let guestsArr = [];
-   let lengthArr = [];
    for (let i = 0; i < allYates.length; i++) {
       if (!sizeArr.includes(allYates[i].model)) {
          sizeArr.push(allYates[i].model);
       }
    }
-   for (let i = 0; i < allYates.length; i++) {
+  for (let i = 0; i < allYates.length; i++) {
       if (!modelArr.includes(allYates[i].make)) {
          modelArr.push(allYates[i].make);
       }
    }
-   for (let i = 0; i < allYates.length; i++) {
-      if (!yearArr.includes(allYates[i].year)) {
-         yearArr.push(allYates[i].year);
-      }
-   }
+
    for (let i = 0; i < allYates.length; i++) {
       if (!guestsArr.includes(allYates[i].guests)) {
          guestsArr.push(allYates[i].guests);
-      }
-   }
-   for (let i = 0; i < allYates.length; i++) {
-      if (!lengthArr.includes(allYates[i].length)) {
-         lengthArr.push(allYates[i].length);
       }
    }
 
@@ -64,9 +51,7 @@ const Filtros = ({ setCurrentPage, setCharging }) => {
       e.preventDefault();
       setSizeSelect(e.target.value);
       setModelSelect('Select');
-      setYearSelect('Select');
       setGuestsSelect('Select');
-      setLengthSelect('Select');
       setCharging(true);
       dispatch(filterBySize(e.target.value));
       setCurrentPage(1);
@@ -79,26 +64,9 @@ const Filtros = ({ setCurrentPage, setCharging }) => {
       e.preventDefault();
       setCharging(true);
       setModelSelect(e.target.value);
-      setSizeSelect('Select');
-      setYearSelect('Select');
       setGuestsSelect('Select');
-      setLengthSelect('Select');
+      setSizeSelect('Select');
       dispatch(filterByModel(e.target.value));
-      setCurrentPage(1);
-      setTimeout(() => {
-         setCharging(false);
-      }, 400);
-   };
-
-   const handleInputYear = (e) => {
-      e.preventDefault();
-      setCharging(true);
-      setYearSelect(e.target.value);
-      setSizeSelect('Select');
-      setModelSelect('Select');
-      setGuestsSelect('Select');
-      setLengthSelect('Select');
-      dispatch(filterByYear(e.target.value));
       setCurrentPage(1);
       setTimeout(() => {
          setCharging(false);
@@ -111,24 +79,7 @@ const Filtros = ({ setCurrentPage, setCharging }) => {
       setGuestsSelect(e.target.value);
       setSizeSelect('Select');
       setModelSelect('Select');
-      setYearSelect('Select');
-      setLengthSelect('Select');
       dispatch(filterByGuests(e.target.value));
-      setCurrentPage(1);
-      setTimeout(() => {
-         setCharging(false);
-      }, 400);
-   };
-
-   const handleInputLength = (e) => {
-      e.preventDefault();
-      setCharging(true);
-      setLengthSelect(e.target.value);
-      setSizeSelect('Select');
-      setModelSelect('Select');
-      setYearSelect('Select');
-      setGuestsSelect('Select');
-      dispatch(filterByLength(e.target.value));
       setCurrentPage(1);
       setTimeout(() => {
          setCharging(false);
@@ -141,9 +92,7 @@ const Filtros = ({ setCurrentPage, setCharging }) => {
       dispatch(getAllProducts());
       setSizeSelect('Select');
       setModelSelect('Select');
-      setYearSelect('Select');
       setGuestsSelect('Select');
-      setLengthSelect('Select');
       setTimeout(() => {
          setCharging(false);
       }, 400);
@@ -154,7 +103,7 @@ const Filtros = ({ setCurrentPage, setCharging }) => {
          <FilterBox>
             <FilterTitle>Model:</FilterTitle>
             <Select value={sizeSelect} onChange={handleInputSize}>
-               <Option value='all'>Select</Option>
+               <Option value='all'>All</Option>
                {sizeArr.length > 0 &&
                   sizeArr
                      .sort((x, y) => {
@@ -179,8 +128,9 @@ const Filtros = ({ setCurrentPage, setCharging }) => {
             <FilterTitle>Make:</FilterTitle>
             <Select onChange={handleInputModel} value={modelSelect}>
                <Option key='none' value='all'>
-                  Select
+                  All
                </Option>
+
                {modelArr.length > 0 &&
                   modelArr.map((model, i) => {
                      return (
@@ -192,36 +142,10 @@ const Filtros = ({ setCurrentPage, setCharging }) => {
             </Select>
          </FilterBox>
          <FilterBox>
-            <FilterTitle>Year:</FilterTitle>
-            <Select onChange={handleInputYear} value={yearSelect}>
-               <Option key='none' value='all'>
-                  Select
-               </Option>
-               {yearArr.length > 0 &&
-                  yearArr
-                     .sort((x, y) => {
-                        if (x > y) {
-                           return 1;
-                        }
-                        if (x < y) {
-                           return -1;
-                        }
-                        return 0;
-                     })
-                     .map((year, i) => {
-                        return (
-                           <Option key={i} value={year}>
-                              {year}
-                           </Option>
-                        );
-                     })}
-            </Select>
-         </FilterBox>
-         <FilterBox>
             <FilterTitle>Guests:</FilterTitle>
             <Select onChange={handleInputGuests} value={guestsSelect}>
                <Option key='none' value='all'>
-                  Select
+                  All
                </Option>
                {guestsArr.length > 0 &&
                   guestsArr.map((guests, i) => {
@@ -231,35 +155,6 @@ const Filtros = ({ setCurrentPage, setCharging }) => {
                         </Option>
                      );
                   })}
-            </Select>
-         </FilterBox>
-         <FilterBox>
-            <FilterTitle>Length:</FilterTitle>
-            <Select onChange={handleInputLength} value={lengthSelect}>
-               <Option key='none' value='all'>
-                  Select
-               </Option>
-               {lengthArr.length > 0 &&
-                  lengthArr
-                     .sort((x, y) => {
-                        if (x > y) {
-                           return 1;
-                        }
-                        if (x < y) {
-                           return -1;
-                        }
-                        return 0;
-                     })
-                     .map((length, i) => {
-                        if (length.length !== 0) {
-                           return (
-                              <Option key={i} value={length}>
-                                 {length}
-                              </Option>
-                           );
-                        }
-                        return;
-                     })}
             </Select>
          </FilterBox>
          <ResetButton onClick={handleReset}>RESET</ResetButton>
